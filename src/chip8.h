30fb4cfcbe7ecd61d27c75f1649121f34bd3bcaa
GauravDhanraja <gauravdhanraja88@proton.me>
2025-10-31 09:38:05 +0530

    update variable name

--- src/chip8.h
+++ src/chip8.h
@@ -2,19 +2,21 @@
 
 class Chip8 {
 public:
-  uint8_t registers[16]{};
-  uint8_t memory[4096]{};
+  uint8_t V[16];
+  uint8_t memory[4096];
 
-  uint16_t index{};
-  uint16_t pc{};
+  uint16_t index;
+  uint16_t pc;
   uint16_t opcode;
 
-  uint16_t stack[16]{};
-  uint8_t sp{};
+  uint16_t stack[16];
+  uint8_t sp;
 
-  uint8_t delay_timer{};
-  uint8_t sound_timer{};
+  uint8_t delay_timer;
+  uint8_t sound_timer;
 
-  uint8_t keypad[16]{};
-  uint32_t video[64 * 32]{};
+  uint8_t key[16];
+  uint32_t video[64 * 32];
+
+  bool drawFlag;
 };
